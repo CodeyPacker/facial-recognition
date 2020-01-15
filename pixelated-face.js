7c2c5dfbe677ec533c8559c456978a8cc01d8298
CodeyPacker <codeypacker@gmail.com>
2020-01-14 22:55:14 -0700

    took a boundingbox snapshot, scaled it down, then back up

--- pixelated-face.js
+++ pixelated-face.js
@@ -1,13 +1,10 @@
 const video = document.querySelector('.webcam');
-
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
-ctx.strokeStyle = '#fff';
-ctx.lineWidth = 2;
-
 const faceCanvas = document.querySelector('.face');
-const faceCtx = canvas.getContext('2d');
-
+const faceCtx = faceCanvas.getContext('2d');
+const size = 10;
+const scale = 1.35;
 // make a new face detector
 const faceDetector = new window.FaceDetector();
 
@@ -30,6 +27,7 @@ async function detect() {
   const faces = await faceDetector.detect(video);
   // ask the browser when the next animation frame is, and tell it to run detect
   faces.forEach(drawFace);
+  faces.forEach(sensor);
   requestAnimationFrame(detect);
 }
 
@@ -39,6 +37,43 @@ function drawFace(face) {
   ctx.strokeStyle = '#fff';
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height);
+}
+
+function sensor({ boundingBox: face }) { // destructure bounding box from face, rename it to face
+  faceCtx.imageSmoothingEnabled = false; // Keeps it pixelated
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  // take a snapshot of the bounding box, but shrink it to 10px
+  // then scale it back up, so it loses img quality/gets pixelated.
+  faceCtx.drawImage(
+    // 5 src args
+    video, // where the src comes from
+    face.x, // where we start
+    face.y,
+    face.width,
+    face.height,
+    // 4 draw args
+    face.x,
+    face.y, // where we should start drawing the x and y
+    size,
+    size
+  )
+
+  const width = face.width * scale;
+  const height = face.height * scale;
+  // draw the small face back on, but scaled up
+  faceCtx.drawImage(
+    // 5 src args
+    faceCanvas, // source
+    face.x,
+    face.y,
+    size,
+    size,
+    // 4 draw args
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2, // where we should start drawing the x and y
+    width,
+    height,
+  )
 }
 
 populateVideo().then(detect);
